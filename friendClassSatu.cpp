12b1c3064c35f35fdfa500e37e9703347678d4c6
riqsans <thrq88@gmail.com>
2024-05-06 15:09:30 +0700

    input data class orang dan siswa

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -2,4 +2,20 @@
 using namespace std;
 
 class siswa;
-class orang;
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+class siswa
+{
+private:
+    int id;
+public:
+    void setId(int pId);
+    void displayAll(orang&a);
+};
